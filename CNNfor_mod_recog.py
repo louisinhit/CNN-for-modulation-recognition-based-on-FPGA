e2f9ff0d82c0dad407ed6b3cb1edf3490b3c6587
louisinhit <30955307+louisinhit@users.noreply.github.com>
2018-10-11 23:51:40 +1100

    Add files via upload

---
+++ CNNfor_mod_recog.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+# python 3 with tensorflow
+#%matplotlib inline
+import os,random
+os.environ["KERAS_BACKEND"] = "tensorflow"
+import numpy as np
+from keras.utils import np_utils
+import keras.models as models
+from keras.layers.core import Reshape,Dense,Dropout,Activation,Flatten
+from keras.layers.noise import GaussianNoise
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D, ZeroPadding3D, Conv1D
+from keras.regularizers import *
+from keras.optimizers import adam
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle, random, sys, keras
+import h5py
+
+from keras.callbacks import Callback
+from sklearn.metrics import f1_score, precision_score, recall_score
+# Load the dataset ...
+#  You will need to seperately download or generate this file
+with open("allinconst.dat",'rb') as xd1:
+    Xd = pickle.load(xd1,encoding='latin1')
+snrs,mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1,0])
+X = []  
+lbl = []
+for mod in mods:
+    for snr in snrs:
+        X.append(Xd[(mod,snr)])
+        for i in range(Xd[(mod,snr)].shape[0]):  lbl.append((mod,snr))
+X = np.vstack(X)
+# Partition the data
+#  into training and test sets of the form we can train/test on 
+#  while keeping SNR and Mod labels handy for each
+np.random.seed(2016)
+n_examples = X.shape[0]
+n_train = n_examples * 0.6  #set the train size
+train_idx = np.random.choice(range(0,n_examples), size=int(n_train), replace=False)
+test_idx = list(set(range(0,n_examples))-set(train_idx))
+X_train = X[train_idx]
+X_test =  X[test_idx]
+def to_onehot(yy):
+    yy1 = np.zeros([len(yy), max(yy)+1])
+    yy1[np.arange(len(yy)),yy] = 1
+    return yy1
+Y_train = to_onehot(list(map(lambda x: mods.index(lbl[x][0]), train_idx)))
+Y_test = to_onehot(list(map(lambda x: mods.index(lbl[x][0]), test_idx)))
+
+in_shp = list(X_train.shape[1:])
+print (X_train.shape, in_shp)
+classes = mods
+
+#%%
+dr = 0.5
+model = models.Sequential()
+model.add(Reshape([1]+in_shp, input_shape=in_shp))
+model.add(Conv2D(64, (3,3),padding='valid', activation='relu', name='conv1', init='glorot_uniform',data_format='channels_first'))
+model.add(Dropout(dr))
+model.add(Conv2D(40, (3,3), padding='valid', activation='relu', name='conv2', init='glorot_uniform',data_format='channels_first'))
+model.add(Dropout(dr))
+model.add(Flatten())
+model.add(Dense(128, activation='relu', init='he_normal', name='dense1'))
+model.add(Dropout(dr))
+model.add(Dense(len(classes), init='he_normal', name='dense2' ))
+model.add(Activation('softmax'))
+model.add(Reshape([len(classes)]))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.summary()
+#%%
+# Set up some params 
+epochs = 200 # number of epochs to train on
+batch_size = 256 # training batch size default1024
+filepath = "CNN_constellation.wts.h5"
+
+history = model.fit(X_train,
+    Y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    verbose=2,
+    validation_data=(X_test, Y_test),
+    callbacks = [ # https://keras-cn.readthedocs.io/en/latest/other/callbacks/
+        keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
+        keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+        #metrics    
+    ]) #EarlyStopping
+model.load_weights(filepath)
+#%%
+score = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch_size)
+print (score)
+#%%
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=[]):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+#%%
+# Plot confusion matrix
+acc = {}
+for snr in snrs:
+
+    test_SNRs = list(map(lambda x: lbl[x][1], test_idx))
+#    print(test_SNRs)
+    test_X_i = X_test[np.where(np.array(test_SNRs)==snr)]
+    test_Y_i = Y_test[np.where(np.array(test_SNRs)==snr)]    
+
+    # estimate classes
+    test_Y_i_hat = model.predict(test_X_i)
+    conf = np.zeros([len(classes),len(classes)])
+    confnorm = np.zeros([len(classes),len(classes)])
+    for i in range(0,test_X_i.shape[0]):
+        j = list(test_Y_i[i,:]).index(1)
+        k = int(np.argmax(test_Y_i_hat[i,:]))
+        conf[j,k] = conf[j,k] + 1
+    for i in range(0,len(classes)):
+        confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+    plt.figure()
+    plot_confusion_matrix(confnorm, labels=classes, title="ConvNet Confusion Matrix (SNR=%d)"%(snr))
+    
+    cor = np.sum(np.diag(conf)) 
+    ncor = np.sum(conf) - cor
+    print ("Overall Accuracy: ", cor / (cor+ncor))
+    acc[snr] = 1.0*cor/(cor+ncor)
+#%%
+# Plot accuracy curve
+plt.figure()
+plt.plot(snrs, list(map(lambda x: acc[x], snrs)))
+plt.xlabel("Signal to Noise Ratio")
+plt.ylabel("Classification Accuracy")
+plt.title("CNN2 Classification Accuracy on constellation")
